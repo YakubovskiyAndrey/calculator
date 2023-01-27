b63d640f374c0bab57cee2b8fe378c5f01aa1394
333 <nelo9997@gmail.com>
2023-01-27 16:02:44 +0200

    initial commit

---
+++ src/components/arithmeticFunctions.js
@@ -0,0 +1,76 @@
+export function getMathHandler() {
+    const math = getMathFn();  
+    let divByZero = false;
+  
+    return applyMath;
+    
+    /***/
+  
+    function applyMath(math_str) {
+      divByZero = false;    
+        
+      let result = parseLinearMath(math_str);
+
+      return divByZero ? "Error division by zero!" : result;
+    }
+    
+    function parseLinearMath(math_str) {
+      math_str = mulOrDiv(math_str);
+      math_str = plusOrMinus(math_str);
+  
+      return math_str;
+  
+      /***/
+  
+      function mulOrDiv(math_str) {
+        let length = (math_str.match(/\/|\*/g) || []).length;
+        if (!length) return math_str;
+  
+        for (let i = 0; i < length; i++) {
+          math_str = math_str.replace(
+            /(\d+(?:\.\d+)?)(\/|\*)(-?\d+(?:\.\d+)?)/,
+            function(_, a, oper, b) {
+              return math(a, oper, b);
+            }
+          );
+        }
+  
+        return math_str;
+      }
+  
+      function plusOrMinus(math_str) {
+        let length = (math_str.match(/\+|-/g) || []).length;
+        if (!length) return math_str;
+  
+        for (let i = 0; i < length; i++) {
+          math_str = math_str.replace(
+            /((?:^-)?\d+(?:\.\d+)?)(\+|-)(\d+(?:\.\d+)?)/,
+            function(_, a, oper, b) {
+              return math(a, oper, b);
+            }
+          );
+        }
+  
+        return math_str;
+      }
+    }
+  
+    function getMathFn() {
+      let local_math = {
+        "+": (a, b) => Number(a) + Number(b),
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => {
+          if( b === "0" ) {
+            divByZero = true;
+          }
+          
+          return (a / b);
+        },
+      };
+  
+      return function math(a, operation, b) {
+        return local_math[operation](a, b);
+      }
+    }
+  }
